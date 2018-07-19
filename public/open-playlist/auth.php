afe7250e024376ea1b91b959c32adbddeaf6d5fe
Matt Harris <mjharris2407@gmail.com>
2018-07-19 07:31:18 -0400

    auth placeholders

---
+++ public/open-playlist/auth.php
@@ -0,0 +1,13 @@
+<?php
+
+// SPOTIFY API APPLICATION CREDENTIALS
+$client_id = "--------------------------------";
+$client_secret = "";
+
+// THE APPLICATION USER AND SELECTED PLAYIST ID
+$open_playlist_userid = "MY-SPOTIFY-USERID";
+$open_playlist_id = "MY-SPOTIFY-USER-PLAYLIST";
+
+// READ THE DOCUMENTATION TO OBTAIN REFRESH TOKEN
+$refresh_token = "";
+
